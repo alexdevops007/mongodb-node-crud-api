2a5cb637d4574303d06d1d209f804438ee9d035d
Alex Simisi Nta <alexsms.devops@gmail.com>
2023-07-26 19:51:05 +0200

    create insertnote function controller

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -2,7 +2,29 @@ const { client, db } = require("../config/dbConfig");
 const notes = client.db("notesdb").collection("notes");
 const { v4: uuidv4 } = require("uuid");
 
-const insertNote = async (req, res, next) => {};
+const insertNote = async (req, res, next) => {
+  const { title, content, tag, isCompleted } = req.body;
+
+  let data = {
+    _id: uuidv4(),
+    title: title,
+    content: content,
+    tag: tag,
+    isCompleted: isCompleted,
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+
+  const insertRecord = await notes.insertOne(data);
+  console.log("insertRecord: ", insertRecord);
+
+  res.status(200).json({
+    statusCode: 200,
+    status: "success",
+    message: "New note inserted successfully",
+    insertRecord: insertRecord
+  });
+};
 
 const readNoteById = async (req, res, next) => {};
 
@@ -19,12 +41,12 @@ const readNotesByTag = async (req, res, next) => {};
 const readNotesByStatus = async (req, res, next) => {};
 
 module.exports = {
-    insertNote,
-    readNoteById,
-    readAllNotes,
-    deleteNoteById,
-    updateNoteById,
-    deleteAllNotes,
-    readNotesByTag,
-    readNotesByStatus
-}
+  insertNote,
+  readNoteById,
+  readAllNotes,
+  deleteNoteById,
+  updateNoteById,
+  deleteAllNotes,
+  readNotesByTag,
+  readNotesByStatus,
+};
